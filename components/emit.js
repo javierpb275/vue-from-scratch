8e5e47dfa0201fbcfe321b3abb5c93bab52dde82
javierpb275 <javierpb275@gmail.com>
2021-04-07 19:35:56 +0200

    used the emit() method that receives 2 parameters, the name of the function that we wanna emit and some data, in this case the carBrand that we have in our component. And in index we pass that function as a propertie in our emit component and we name that function whatever we want and in the methods object we use taht function wich we pass it the data that we pass in that component (carbrand) and we can do whatever we want with it

--- components/emit.js
+++ components/emit.js
@@ -7,7 +7,9 @@ Vue.component('emit', {
     template: `
     <div>
         <h1>Sending out events with Vuejs 2</h1>
-        <p @click="">Click here to send out an event to the instance ROOT of Vuejs</p>
+        <p @click="$emit('showcarbrand', carBrand)">
+        Click here to send out an event to the instance ROOT of Vuejs
+        </p>
     </div>
     `
 });
