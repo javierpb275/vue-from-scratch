00fe3d585179baca8a11223734cf909fa1facfa0
javierpb275 <javierpb275@gmail.com>
2021-04-07 20:08:00 +0200

    accessing the data from our children. mounted() is a function that gets executed everytime the component is mounted

---
+++ components/child-data.js
@@ -0,0 +1,12 @@
+Vue.component('child-data', {
+    data() {
+        return {
+            cmpName: 'Child Data CMP'
+        }
+    },
+    template:`
+    <div>
+    <h2>Accessing data from child component</h2> 
+    </div>
+    `
+});
