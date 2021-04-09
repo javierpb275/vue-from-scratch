ef0ef3ad707078f15bb40fe35def04b92d53b5cb
javierpb275 <javierpb275@gmail.com>
2021-04-09 16:42:10 +0200

    created loops component to loop through an array and display the elements in a ul

---
+++ components/loops.js
@@ -0,0 +1,24 @@
+Vue.component('loops', {
+    data() {
+        return {
+            frameworks: [
+                {id: 1, name: 'Vuejs 2'},
+                {id: 2, name: 'Reactjs'},
+                {id: 3, name: 'Ember'},
+                {id: 4, name: 'Adonisjs'},
+                {id: 5, name: 'Angular'},
+                {id: 6, name: 'Laravel'},
+            ]
+        }
+    },
+    template:`
+    <div>
+        <h1>Loops with v-for</h1>
+        <ul v-if="frameworks.length">
+            <li v-for="framework in frameworks" :key="framework.id">
+                {{ framework.name }}
+            </li>
+        </ul>
+    </div>
+    `
+}) 
