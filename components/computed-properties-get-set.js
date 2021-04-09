262aaf47677076fae0a78a5f9282932c59ff6df5
javierpb275 <javierpb275@gmail.com>
2021-04-09 17:12:46 +0200

    using getters and setters with computed-properties

---
+++ components/computed-properties-get-set.js
@@ -0,0 +1,24 @@
+Vue.component('computed-properties-get-set', {
+    data() {
+        return {
+            amount: 0
+        }
+    },
+    computed: {
+        amountFormatted: {
+            get() {
+                return `$${this.amount}`;
+            },
+            set(newValue) {
+                this.amount = newValue;
+            }
+        } 
+    },
+    template: `
+    <div>
+        <h1>Computed Properties gettters and setters</h1>
+        <input v-model="amount"/>
+        <p>{{amountFormatted}}</p>
+    </div>
+    `
+}); 
