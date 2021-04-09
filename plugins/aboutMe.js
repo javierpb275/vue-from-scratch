75b7c51a2a33ccf77b952020b83905d86d9907ed
javierpb275 <javierpb275@gmail.com>
2021-04-09 20:52:27 +0200

    Vue.prototype.$me: overwrite the prototype: I will write sth about me. We are extending the prototype of vue.
    $me = (name, age): these will be dynamic from outside. and job we get it from inside.

--- plugins/aboutMe.js
+++ plugins/aboutMe.js
@@ -1,9 +1,12 @@
 const AboutMe = {
     install: (Vue, options) => {
-        
+        const { job } = options;
+        Vue.prototype.$me = (name, age) => {
+            return `Mi name is ${name}, I'm ${age} years old. My job is: ${job}`;
+        }
     }
 }
 
 Vue.use(AboutMe, {
-
-})
+    job: 'developer'
+});
