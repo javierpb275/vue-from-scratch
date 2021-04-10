053db1a234617f4d4000482ed50b49af4b2845cd
javierpb275 <javierpb275@gmail.com>
2021-04-10 10:51:19 +0200

    more complex way to define props. if we wanna pass a number we have to add the : ' :age="37" '

--- components/props.js
+++ components/props.js
@@ -1,9 +1,22 @@
 Vue.component('props', {
-    props: ['name', 'surname'],
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        surname: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        }
+    },
     template:`
     <div>
         <h2>Props with Vuejs 2</h2>
-        <p>{{name}} {{surname}}</p>
+        <p>{{name}} {{surname}}, Age: {{age}}</p>
     </div>
     `
 });
